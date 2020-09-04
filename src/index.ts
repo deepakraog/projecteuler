4da19556f897fac19c3fd35bf9931483f37a6c5e
deepakraog <dgaikwad@cleo.com>
2020-09-05 01:15:20 +0530

    problem 1 solution

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 "use strict";
-console.log('Hello world!');
+
+import {Multiples3x5} from "./mul3x5";
+
+let src = new Multiples3x5();
+console.log(`Result of Euler Problem 1 - Multiple Sum of 5, 3 below 1000 is ::: ${src.findWhole()}`);
---
+++ src/mul3x5.ts
@@ -0,0 +1,20 @@
+export class Multiples3x5 {
+
+    findWhole(): number {
+        let sum = 0;
+        let i = 1;
+        let flag = false;
+        while (i < 1000) {
+            let flag = false;
+            if (i % 3 == 0) {
+                sum += i;
+                flag = true;
+            }
+            if (i % 5 == 0 && !flag) {
+                sum += i;
+            }
+            i++;
+        }
+        return sum;
+    }
+}
